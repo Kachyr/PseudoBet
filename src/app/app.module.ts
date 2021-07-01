5888e391e144d32ebd013c8c7d44f39b17ef058a
Oleksandr Kachan <kach_work@ukr.net>
2021-07-01 22:58:49 +0300

    added placeholders and proxy config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { TradingInterfaceComponent } from './Components/trading-interface/trading-interface.component';
 import { HistoryComponent } from './Components/history/history.component';
+import { ChartComponent } from './Components/trading-interface/chart/chart.component';
+import { PurchaseFormComponent } from './Components/trading-interface/purchase-form/purchase-form.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, TradingInterfaceComponent, HistoryComponent],
+  declarations: [AppComponent, HeaderComponent, TradingInterfaceComponent, HistoryComponent, ChartComponent, PurchaseFormComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
