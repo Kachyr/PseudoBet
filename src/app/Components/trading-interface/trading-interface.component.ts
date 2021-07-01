caa6ecac3c77e83e90963f0b48f1370b498ae851
Oleksandr Kachan <kach_work@ukr.net>
2021-07-01 12:48:24 +0300

    Added top level components

---
+++ src/app/Components/trading-interface/trading-interface.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trading-interface',
+  templateUrl: './trading-interface.component.html',
+  styleUrls: ['./trading-interface.component.css']
+})
+export class TradingInterfaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
