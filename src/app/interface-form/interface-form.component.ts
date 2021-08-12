606579d19788de072fbb8c04469f0d0548e3a44b
Oleksandr Kachan <kach_work@ukr.net>
2021-08-12 11:28:15 +0300

    fixed coments

--- src/app/interface-form/interface-form.component.ts
+++ src/app/interface-form/interface-form.component.ts
@@ -1,28 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-interface-form',
   templateUrl: './interface-form.component.html',
   styleUrls: ['./interface-form.component.css'],
 })
-export class InterfaceFormComponent implements OnInit {
-  betForm!: FormGroup;
-  stock: number = 300;
-  minBet: number = 10;
+export class InterfaceFormComponent {
+  userDeposit = 300;
+  minBet = 10;
+
+  betForm = new FormGroup({
+    team: new FormControl(null, Validators.required),
+    amount: new FormControl(null, [
+      Validators.required,
+      Validators.min(this.minBet),
+      Validators.max(this.userDeposit),
+    ]),
+  });
 
   constructor() {}
-
-  ngOnInit() {
-    this.betForm = new FormGroup({
-      team: new FormControl(null, Validators.required),
-      amount: new FormControl(0, [
-        Validators.required,
-        Validators.max(this.stock),
-        Validators.min(this.minBet),
-      ]),
-    });
-  }
 
   onSubmit(values: FormGroup) {
     console.log(values);
