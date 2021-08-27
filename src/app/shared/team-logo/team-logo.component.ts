5f298f23ee948e7257b65fcf89d74b708d9796f4
Oleksandr Kachan <kach_work@ukr.net>
2021-08-27 13:27:32 +0300

    extending the functionality of the component

--- src/app/shared/team-logo/team-logo.component.ts
+++ src/app/shared/team-logo/team-logo.component.ts
@@ -1,12 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MemoryCacheService } from 'src/app/memory-сache/memory-cache.service';
 
 @Component({
   selector: 'app-team-logo',
   templateUrl: './team-logo.component.html',
   styleUrls: ['./team-logo.component.css'],
 })
-export class TeamLogoComponent {
+export class TeamLogoComponent implements OnInit, OnDestroy {
+  @Input() teamId!: number;
   @Input() name!: string;
   @Input() iconUrl!: string;
   @Input() unchecked = false;
+
+  private cacheSubscription!: Subscription;
+
+  constructor(private cacheService: MemoryCacheService) {}
+
+  ngOnInit() {
+    if (this.teamId) {
+      this.cacheSubscription = this.cacheService
+        .getTeamById(this.teamId)
+        .subscribe((team) => {
+          if (team) {
+            this.iconUrl = team.iconUrl;
+            this.name = team.name;
+          }
+        });
+    }
+  }
+
+  ngOnDestroy() {
+    this.cacheSubscription.unsubscribe();
+  }
 }
