7ef00e2b10043c6b6231255dd7b2a27a337fbb05
Oleksandr Kachan <kach_work@ukr.net>
2021-09-23 15:25:51 +0300

    chart-data flow

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { TimerService } from './../shared/timer/timer.service';
+import { Component, OnInit } from '@angular/core';
+import { repeat } from 'rxjs/operators';
+import { DataRepository } from '../repository/repository.service';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css'],
 })
-export class ChartComponent {}
+export class ChartComponent implements OnInit {
+  timerData?: {
+    passed: number;
+    left: number;
+  };
+  lastReq!: number;
+  constructor(
+    private repo: DataRepository,
+    private timerService: TimerService,
+  ) {}
+
+  ngOnInit(): void {
+    // TODO: implement visualization of data
+    this.timerService.runningTimer.subscribe((timerData) => {
+      if (timerData.passed % 5 === 0 && timerData.passed > 0) {
+        this.repo.getChartData(this.lastReq).subscribe((data) => {
+          console.log(data);
+        });
+        this.lastReq = Date.now();
+        console.log(timerData.passed);
+      }
+    });
+  }
+}
--- src/app/repository/repository.service.ts
+++ src/app/repository/repository.service.ts
@@ -1,3 +1,4 @@
+import { ChartData } from './../shared/models/chart.model';
 import { Team } from './../shared/models/team.model';
 import { Bet } from './../shared/models/bet.model';
 import { Game } from './../shared/models/game.model';
@@ -12,6 +13,28 @@ import TEAMS from '../mocks/teams/teams.json';
   providedIn: 'root',
 })
 export class DataRepository {
+  /**
+   * Method will return array with data for char,
+   * number of elements depends depends on difference last request time and current moment
+   * @param lastRequestDateTime time of last request in millisecond format
+   * @returns Observable with array of chart data objects
+   */
+  getChartData(lastRequestDateTime: number): Observable<ChartData[]> {
+    const dataArr: ChartData[] = [];
+    const now = Date.now();
+
+    const timeDifference = lastRequestDateTime
+      ? Math.round((now - lastRequestDateTime) / 1000)
+      : 5;
+    //generate objects for chart
+    for (let i = 0; i < timeDifference; i++) {
+      dataArr.push(this.generateChartObject(i));
+    }
+    //set last request time
+
+    return of(dataArr).pipe(delay(500));
+  }
+
   getTeams(): Observable<Team[]> {
     return of(TEAMS).pipe(delay(1000));
   }
@@ -50,5 +73,12 @@ export class DataRepository {
     const randomInteger = Math.round(Math.random() * 10);
     const seconds = randomInteger > 5 ? 10000 : -10000;
     return new Date(now - seconds);
+  }
+
+  private generateChartObject(increment: number): ChartData {
+    const time = Date.now() + increment;
+    const randomValue = Math.round(Math.random() * 10);
+
+    return { time: new Date(time), value: randomValue };
   }
 }
